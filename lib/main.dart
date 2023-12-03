a93dc24b379d291bff546f0179b2248457b27fb6
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-03 10:21:06 -0500

    interface2

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:billapp/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
